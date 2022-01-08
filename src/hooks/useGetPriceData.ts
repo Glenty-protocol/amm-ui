1323adb726739816cb9262e86194a5a5e74d06c9
maim228 <egyreferral@gmail.com>
2022-01-09 01:28:18 +0200

    add multi tokens & fix live price multi loading

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -11,25 +11,26 @@ type ApiResponse = {
 }
 
 const useGetPriceData = () => {
-  const [data, setData] = useState<number>(0)
-  const [gValue, setG] = useState<number>(0)
-  const [bValue, setB] = useState<number>(0)
-  const [bUsd, setU] = useState<number>(0)
+  const [data, setData] = useState(0)
+  const [gValue, setG] = useState(0)
+  const [bValue, setB] = useState(0)
+  const [bUsd, setU] = useState(0)
   const multicallContract = useMulticallContract();
 
   useEffect(() => {
-    const fetchdata=()=>{
-      fetch('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x97769506e36988d08745b64e78f66e37858f14e9&address=0xfcdf033e55ca6a62300337d88da1810933a54f82&tag=latest&apikey=G9GR47NUZ9B32CHEYFQE7N99Z5V54V1SC8')
+    const fetchdata=async ()=>{
+      await fetch('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x97769506e36988d08745b64e78f66e37858f14e9&address=0xfcdf033e55ca6a62300337d88da1810933a54f82&tag=latest&apikey=G9GR47NUZ9B32CHEYFQE7N99Z5V54V1SC8')
       .then(res=>res.json())
       .then(glenty=>setG(glenty.result/1000000000000000000))
-      fetch('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c&address=0xfcdf033e55ca6a62300337d88da1810933a54f82&tag=latest&apikey=G9GR47NUZ9B32CHEYFQE7N99Z5V54V1SC8')
+      await fetch('https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c&address=0xfcdf033e55ca6a62300337d88da1810933a54f82&tag=latest&apikey=G9GR47NUZ9B32CHEYFQE7N99Z5V54V1SC8')
       .then(res=>res.json())
       .then(bnb=>setB(bnb.result/1000000000000000000))
-    fetch('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd')
+      await fetch('https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd')
       .then(res=>res.json())
       .then(usd=>setU(usd.binancecoin.usd))
     
-    setData((bValue/gValue)*bUsd)  
+    setData((bValue/gValue)*bUsd);
+    console.log((bValue/gValue)*bUsd)
     }
     fetchdata();
   },[gValue,bValue,bUsd]);
